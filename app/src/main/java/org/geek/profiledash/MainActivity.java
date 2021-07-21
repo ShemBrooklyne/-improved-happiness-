fe96b7c0a637ee51b89ffc46e82e12a921548c1e
ShemBrooklyne <shemnyatti@gmail.com>
2021-07-21 16:25:33 +0300

    Sign In UI

--- app/src/main/java/org/geek/profiledash/MainActivity.java
+++ app/src/main/java/org/geek/profiledash/MainActivity.java
@@ -2,13 +2,33 @@ package org.geek.profiledash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.button)
+    Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        signin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == signin) {
+            Intent intent = new Intent(this, SignInActivity.class);
+            startActivity(intent);
+        }
     }
 }
