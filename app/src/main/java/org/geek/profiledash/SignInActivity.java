202e28a2858ee2d20711b7bd6395debc44128798
ShemBrooklyne <shemnyatti@gmail.com>
2021-07-21 17:14:21 +0300

    sign up ui

--- app/src/main/java/org/geek/profiledash/SignInActivity.java
+++ app/src/main/java/org/geek/profiledash/SignInActivity.java
@@ -2,13 +2,33 @@ package org.geek.profiledash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SignInActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.createTxt)
+    TextView createAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        ButterKnife.bind(this);
+
+        createAcc.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == createAcc) {
+            Intent intent = new Intent(this, SignUpActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/org/geek/profiledash/SignUpActivity.java
+++ app/src/main/java/org/geek/profiledash/SignUpActivity.java
@@ -2,13 +2,33 @@ package org.geek.profiledash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.loginTxt)
+    TextView loginAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        loginAcc.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == loginAcc) {
+            Intent intent = new Intent(this, SignInActivity.class);
+            startActivity(intent);
+        }
     }
 }
